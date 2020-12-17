dd4f348af8331f8c8b8fa652f0e339a6c8b1a96b
Tyrone Chris Abad <tyronechris67@gmail.com>
2020-12-17 17:34:45 +0800

    fix naming

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,9 @@
-const { resolve } = require("path");
-
 module.exports = {
   siteMetadata: {
     title: `Tribox Design`,
     description: `Identify brands, simplify the complex.`,
-    author: `@gatsbyjs`,
+    author: `@tyronskiii`,
+    siteUrl: `https://dev-tribox-website-v3.netlify.app/`,
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
@@ -22,13 +21,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Tribox Design`,
+        short_name: `Tribox`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tribox-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-json`,
@@ -36,6 +35,36 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: './src/data'
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [".mdx", ".md"],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-vscode`,
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+          },
+          {
+            resolve: `gatsby-remark-smartypants`,
+          },
+        ],
+        plugins: [`gatsby-remark-images`],
       },
     }
   ],
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Header from "./Header"
-import { GlobalStyle } from './styles/GlobalStyles'
+import { GlobalStyle } from "./styles/GlobalStyles"
 
 const Layout = ({ children }) => {
 
