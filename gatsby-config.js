cdfd2e24c5980659b3245cd1eb72f39a1f5a4234
Tyrone Chris Abad <tyronechris67@gmail.com>
2021-03-11 17:59:33 +0800

    fix build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,12 +12,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
