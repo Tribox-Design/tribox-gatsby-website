fbbf285c4e2a4c0b0ee6711cb5f55f8ff4c21101
Tyrone Chris Abad <tyronechris67@gmail.com>
2021-04-08 14:53:54 +0800

    color

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,7 @@ const seoKeywords =
   "Brand Positioning, Label/Packaging Design, Brand Messaging, Sound Branding, Brand Identity Design, Digital Marketing, Brand Naming, Illustration, Brand Evaluation Research, Web/Mobile App Development"
 
 const IndexPage = () => (
-  <Layout page="About">
+  <Layout page="About" headerBackground="#000000" footerBackground="#000000">
     <SEO
       title="About Us"
       description="Tribox Design and Advertising OPCorp. is a Cebu based Branding and Advertising agency founded in 2019 by three entrepreneurs and creatives, Inusentes A. Catapusan Jr. (CEO), Regine Marie V. Ylaya (COO), and Tyrone Chris B. Abad (CTO). The company’s line of business includes Brand Strategy, Brand Identity, Graphic Design, Web Development, and Mobile Development. Our mission is to promote the growth of local SMEs and startup businesses through strategy and creativity. We aim to level the playing field for artists and entrepreneurs so they can express their creativity, enhance their craft, and fuel their passion to achieve their individual goals. We envision a community of artists and entrepreneurs driven by their aspirations and passion for their craft to unlock opportunities and attain success."
