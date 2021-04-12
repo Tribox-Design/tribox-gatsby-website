fe6aae790897c238979099119784d6428727d25d
Tyrone Chris Abad <tyronechris67@gmail.com>
2021-04-12 19:56:42 +0800

    Update OurServicesData.js

--- src/data/OurServicesData.js
+++ src/data/OurServicesData.js
@@ -1,12 +1,12 @@
 export const ourServicesData = [
   { service: "Brand Positioning" },
-  { service: "Label/Packaging Design" },
   { service: "Brand Messaging" },
-  { service: "Sound Branding" },
   { service: "Brand Identity Design" },
-  { service: "Digital Marketing" },
   { service: "Brand Naming" },
-  { service: "Illustration" },
   { service: "Brand Evaluation Research" },
+  { service: "Label/Packaging Design" },
+  { service: "Sound Branding" },
+  { service: "Digital Marketing" },
+  { service: "Illustration" },
   { service: "Web/Mobile App Development" }
 ]
