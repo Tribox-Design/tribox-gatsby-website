e16794de148bd0a2c9c6137b09a528230dcc01d5
Tyrone Chris Abad <tyronechris67@gmail.com>
2022-08-13 16:31:27 +0800

    feat: add font page on navbar

--- src/data/MenuData.js
+++ src/data/MenuData.js
@@ -1,5 +1,6 @@
 export const menuData = [
   { title: "Work", link: "/works/" },
+  { title: "Fonts", link: "/fonts/" },
   { title: "About", link: "/about/" },
   { title: "News", link: "/news/" },
   { title: "Contact", link: "/contact/" },
