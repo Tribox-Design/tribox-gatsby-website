843ec42440ed9cdb91162ab094be266326c19e96
Tyrone Chris Abad <tyronechris67@gmail.com>
2023-02-04 07:48:52 +0000

    fix: padding

--- src/components/ManifestoPage/ManifestoContent.js
+++ src/components/ManifestoPage/ManifestoContent.js
@@ -57,10 +57,10 @@ const ServiceWrapper = styled.div`
 const NumberContainer = styled.div`
   width: 20%;
   padding-right: 2rem;
-  padding-left: 4rem;
+  padding-left: 6rem;
 
   @media screen and (max-width: 617px) {
-    padding-right: 1rem;
+    padding-right: 2rem;
     padding-left: 2rem;
   }
 `
