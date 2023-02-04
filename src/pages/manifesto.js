dae76ac7659120584da2a4d4346459a71d9963ff
Tyrone Chris Abad <tyronechris67@gmail.com>
2023-02-04 04:22:54 +0000

    feat: add manifesto content

--- src/pages/manifesto.js
+++ src/pages/manifesto.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import AllFonts from "../components/FontsPage/AllFonts"
 import TriboxLogo from "../images/tribox-design-logo.jpg"
 import IntroManifesto from "../components/ManifestoPage/IntroManifesto"
+import ManifestoContent from "../components/ManifestoPage/ManifestoContent"
 
 const seoKeywords =
   "work, strategy, development, design, marketing, online, identity, digital marketing, cebu, brand, branding, branding in cebu, social media marketing, advertising agency, agency, logo design, best logo in cebu, online, identity, boost, sales, business in cebu, grow, local"
@@ -18,6 +18,7 @@ const IndexPage = () => (
     />
     <IntroManifesto />
     <Line />
+    <ManifestoContent />
   </Layout>
 )
 
