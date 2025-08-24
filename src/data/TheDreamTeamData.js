e2b37516e406f762c98adc5c20617836f384865e
Tribox Design <hello@triboxdesign.com>
2025-08-24 19:42:24 +0800

    Update TheDreamTeamData.js
    temporary

--- src/data/TheDreamTeamData.js
+++ src/data/TheDreamTeamData.js
@@ -5,13 +5,13 @@ export const theDreamTeamData = [
     linkedin: "https://www.linkedin.com/in/inusentes-catapusan-jr-b89b5a143/",
   },
   {
-    name: "Regine Ylaya",
-    position: "Co-Founder/COO",
-    linkedin: "https://www.linkedin.com/in/regineylaya/",
+    //name: "Regine Ylaya",
+    //position: "Co-Founder/COO",
+    //linkedin: "https://www.linkedin.com/in/regineylaya/",
   },
   {
-    name: "Michael Relativo",
-    position: "Business Coach",
-    linkedin: "https://www.linkedin.com/in/michael-relativo-080b7052/",
+    //name: "Michael Relativo",
+    //position: "Business Coach",
+    //linkedin: "https://www.linkedin.com/in/michael-relativo-080b7052/",
   },
 ]
